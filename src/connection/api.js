8af1b8b4047985ea7fcbc0a1127d4718677ddfbf
LAPTOP-R3VEKGVF\dauser <jacob35lol@gmail.com>
2025-03-30 07:55:07 -0700

    API gateway for End Users

--- src/connection/api.js
+++ src/connection/api.js
@@ -2,17 +2,16 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "http://localhost:5000",
+  headers: { "Content-Type": "application/json" },
 });
 
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("authToken");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
+// Automatically attach JWT to all requests
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("jwt");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
 
 export default api;
